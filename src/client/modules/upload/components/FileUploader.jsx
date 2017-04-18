8e247fc5a671c9f772c0dc557d1fd4e66db833d7
bmavity <brian@brianmavity.com>
2017-04-17 22:18:10 -0500

    File Information section now displays a message until it can upload

--- src/client/modules/upload/components/FileUploader.jsx
+++ src/client/modules/upload/components/FileUploader.jsx
@@ -1,21 +1,39 @@
 // @flow
-import React, { Component } from 'react'
+import React, { Component, PropTypes } from 'react'
 
 import styles from './FileUploader.css'
 
 
+const CannotUpload = () =>
+  <div>
+    <h4>Cannot Upload</h4>
+    <p>
+      You must have an IOTA balance and a payment address to process uploads.
+    </p>
+  </div>
+
+const CanUpload = () =>
+  <div>
+    <button id="select-files">Select Files</button>
+  </div>
+
+
 export default class WalletSidebar extends Component {
   static propTypes = {
+    companySeed: PropTypes.string,
+    paymentAddress: PropTypes.string,
   }
 
   state = {
   }
 
   render() {
+    const canUpload = this.props.paymentAddress && this.props.companySeed
+    const DisplayComponent = canUpload ? CanUpload : CannotUpload
     return (
       <div className={styles.fileUploader}>
         <h3>File Information</h3>
-        <button id="select-files">Select Files</button>
+        <DisplayComponent />
       </div>
     )
   }
